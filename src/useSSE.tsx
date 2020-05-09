ca2921da6bc50fde762ecab8409179ae2a78431a
Kasper Moskwiak <kasper.moskwiak@gmail.com>
2020-05-09 23:49:52 +0200

    fix ts

--- src/useSSE.tsx
+++ src/useSSE.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 export const DataContext = React.createContext({});
 
 import { useContext, useState, useEffect, DependencyList, Props } from "react";
